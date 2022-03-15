f50db638d0ef2de4ab5bba3f0af31957e4f3d49d
Ryan C. Durham <ryan@stagerightlabs.com>
2022-03-15 11:57:12 -0700

    Disable Router

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 import './assets/tailwind.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.mount('#app')
